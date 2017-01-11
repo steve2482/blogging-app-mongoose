f73b698b6564b76d2139449f8a54ba14d52b82f2
steve2482 <stevecb12684@yahoo.com>
2017-01-10 20:48:23 -0500

    added basic GET request

--- server.js
+++ server.js
@@ -5,9 +5,25 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const {PORT, DATABASE_URL} = require('./config');
-const {blogPosts} = require('./models');
+const {BlogPosts} = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
 
-
+app.get('/posts', (req, res) => {
+  BlogPosts
+  .find()
+  .limit(10)
+  .exec()
+  .then(posts => {
+    res.json({
+      posts: posts.map(
+        post => post.apiRepresentation())
+    });
+  })
+  .catch(
+    err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
