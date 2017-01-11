c800954c8d6493106655825d711b4d2ea1d9fcaa
steve2482 <stevecb12684@yahoo.com>
2017-01-10 20:22:37 -0500

    schema for blog posts, api data representaion

---
+++ models.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+// schema to represent a blog post
+const blogPostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true}
+  }
+});
+
+// creating virtuals
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogPostSchema.methods.apiRepresentation = function() {
+  return {
+    id: this.id,
+    title: this.title,
+    content: this.content,
+    author: this.authorName
+  };
+};
+
+const BlogPosts = mongoose.model('BlogPosts', blogPostSchema);
+
+module.exports = {BlogPosts};
