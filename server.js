7848387254175315ac01792b15279604ff14110e
steve2482 <stevecb12684@yahoo.com>
2017-01-12 11:10:41 -0500

    run and close server functions added

--- server.js
+++ server.js
@@ -108,3 +108,44 @@ app.delete('/posts/:id', (req, res) => {
       res.status(500).json({message: 'Internal server error'});
     });
 });
+
+let server;
+
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect()
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
